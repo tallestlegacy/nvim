c41d04ad2d9eec90e6e330ec04ce02c3578d052b
TallestLegacy <tallestlegacy@gmail.com>
2023-11-01 16:05:21 +0300

    initial commit (options and mappings)

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,36 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap SPACE as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
---
+++ lua/user/options.lua
@@ -0,0 +1,36 @@
+local options = {
+	-- numberline
+	number = true,
+  numberwidth = 4,
+  relativenumber = false,
+
+	-- editor
+	shiftwidth = 2,
+  tabstop = 2,
+	expandtab = true,
+	smartcase = true,
+	smartindent = true,
+	cursorline = false,
+
+	-- statusline
+	showmode = true,
+	showtabline = 2,
+
+	-- window
+	splitbelow = true,
+	splitright = true,
+
+	-- other
+	timeoutlen = 1000,
+	undofile = true,
+	updatetime = 300,
+	writebackup = false,
+
+}
+
+
+
+-- update options
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
