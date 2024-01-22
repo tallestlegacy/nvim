dc93bdd1f0f9112003df2e908d4e9c172fe03a8c
TallestLegacy <tallestlegacy@gmail.com>
2024-01-22 23:10:43 +0300

    arch init

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -29,7 +29,7 @@ return {
 			-- local anya = require("builtin.ui.anya")
 			local bemo = require("builtin.ui.adventure-time").bemo
 
-			dashboard.section.header.val = bemo
+			dashboard.section.header.val = vim_dos_rebel
 
 			dashboard.section.buttons.val = {
 				dashboard.button("f", "> find file", ":Telescope find_files <CR>"),
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,4 +9,10 @@ return {
 		},
 		opts = {},
 	},
+	{
+		"karb94/neoscroll.nvim",
+		config = function()
+			require("neoscroll").setup({})
+		end,
+	},
 }
