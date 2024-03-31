b843d1d1294b9829515795fbb9df940d59ed3bb1
TallestLegacy <tallestlegacy@gmail.com>
2024-03-31 18:39:35 +0300

    removed codeium ai completions

--- lua/plugins/devtools/init.lua
+++ lua/plugins/devtools/init.lua
@@ -1,5 +1,33 @@
 return {
-	require("plugins.devtools.flutter"),
+	{
+		"akinsho/flutter-tools.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim", -- optional for vim.ui.select
+		},
+		config = function()
+			require("flutter-tools").setup({
+				ui = {
+					border = "rounded",
+				},
+				widget_guides = {
+					enabled = true,
+				},
+			})
+
+			require("which-key").register({
+				f = {
+					l = {
+						"<cmd>lua require('telescope').extensions.flutter.commands()<cr>",
+						"Flutter Tools",
+					},
+				},
+			}, { prefix = "<leader>" })
+		end,
+	},
+
 	-- Go
 	{
 		"ray-x/go.nvim",
