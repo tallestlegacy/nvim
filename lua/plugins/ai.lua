aea4d09bde47255230ecdd0f4a68db5f1a7593c0
TallestLegacy <tallestlegacy@gmail.com>
2024-10-06 09:32:32 +0300

    ai enhancements

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -6,6 +6,9 @@ return {
     config = function()
       -- require("codeium").setup({})
       vim.keymap.set("i", "<Tab>", vim.fn["codeium#Accept"], { expr = true, silent = true })
+      -- vim.g.codeium_enabled = false
+      -- vim.g.codeium_manual = true
+      vim.g.codeium_idle_delay = 100
     end,
   },
 }
