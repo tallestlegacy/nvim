c00c99f72ede78fcbe416c9299a27ecbaeb31861
TallestLegacy <tallestlegacy@gmail.com>
2025-01-05 14:36:42 +0300

    edit formatting and version bump
    removed editor config

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,45 +1,35 @@
 -- temporarily disabled
-
 return {
-  -- Completions
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    'neovim/nvim-lspconfig',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/nvim-cmp',
-    -- luasnip dependencies
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-  },
-  config = function()
-    local cmp = require("cmp")
+    -- Completions
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path', 'hrsh7th/nvim-cmp', -- luasnip dependencies
+        'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
+        "rafamadriz/friendly-snippets"
+    },
+    config = function()
+        local cmp = require("cmp")
 
-    cmp.setup(
-      {
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end
-        },
-        window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        }),
-        sources = cmp.config.sources({
-          { name = "supermaven" },
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end
+            },
+            window = {
+                -- completion = cmp.config.window.bordered(),
+                -- documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({select = false})
+            }),
+            sources = cmp.config.sources({
+                {name = "supermaven"}, {name = "nvim_lsp"}, {name = "luasnip"},
+                {name = "buffer"}, {name = "path"}
+            })
         })
-      }
-    )
-  end
+    end
 }
--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -12,7 +12,7 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        lua = { "luaformatter", lsp_format = "fallback" },
+        lua = { "lua-format", lsp_format = "fallback" },
         javascript = pretty,
         javascriptreact = pretty,
         typescript = pretty,
